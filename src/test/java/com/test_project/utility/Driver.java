cd283e2f83592c0cb259f9003bd8851fd701555f
emp9wer <kirillkandakov@gmail.com>
2023-11-07 23:06:19 -0500

    created frame

--- src/test/java/com/test_project/utility/Driver.java
+++ src/test/java/com/test_project/utility/Driver.java
@@ -9,36 +9,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.time.Duration;
 
 public class Driver {
-//encapsulation
-    //create a private constructor to remove access to this object
-    private Driver() {
+    private Driver() {}
 
-    }
-
-    /*
-    We make the Webdriver private because we want to close access from outside the class.
-    We making it static because we will use it in the static method.
-     */
     private static WebDriver driver; //default value = null
 
-    /*
-    create re-usable utility method which will return the same driver instance once we call it
-    -if an instance doesn't exist it will create first and then it will always return same instance
-     */
-    public static WebDriver getDriver(){
 
-        if (driver==null){
-            /*
-            we will read browser type from configuration.properties file
-            this way we control which browser is opened from outside our code
-             */
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
             String browseType = ConfigurationReader.getProperty("browser");
 
-            /*
-              we will read browser type from configuration.properties file
-              and depending on it - switch statement will determine the case and open matching browser
-             */
-            switch (browseType){
+            switch (browseType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
@@ -62,15 +44,14 @@ public class Driver {
         return driver;
     }
 
-    public static void closeDriver(){
-        if(driver!=null){
-            //this line will terminate currently existing driver completely - it will be not existent going forward
+    public static void closeDriver() {
+        if (driver != null) {
+
             driver.quit();
-            //we assign value back to null so that my singleton can create a newer one if needed
-            driver=null;
+
+            driver = null;
         }
     }
-
 
 
 }
